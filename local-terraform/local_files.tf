a5fb584ed33acb32c258d5a3cac693bd949c65f7
owenwilson <owenwilson_m_f@hotmail.com>
2026-07-21 00:52:35 -0400

    feat: add random provider

--- local-terraform/local_files.tf
+++ local-terraform/local_files.tf
@@ -1,4 +1,6 @@
 resource "local_file" "products" {
+  count = 3
   content = "list of products for next month"
-  filename = "products.txt" 
+  filename = "products-${random_string.sufijo[count.index].id}.txt" 
 }
+
