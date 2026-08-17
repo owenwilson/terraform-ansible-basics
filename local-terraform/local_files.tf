b01e844312525871e568cbc008ab01155af93389
owenwilson <owenwilson_m_f@hotmail.com>
2026-08-17 00:05:43 -0400

    feat: add count with sufijo

--- local-terraform/local_files.tf
+++ local-terraform/local_files.tf
@@ -1,6 +1,6 @@
 resource "local_file" "products" {
-  count = 3
-  content = "list of products for next month"
-  filename = "products-${random_string.sufijo[count.index].id}.txt" 
+  count    = 3
+  content  = "list of products for next month"
+  filename = "products-${random_string.sufijo[count.index].id}.txt"
 }
 
--- local-terraform/random.tf
+++ local-terraform/random.tf
@@ -1,7 +1,7 @@
 resource "random_string" "sufijo" {
-  count = 3
-  length = 4
+  count   = 3
+  length  = 4
   special = false
-  upper = false
+  upper   = false
   numeric = false
 }
